9604c520a41f922f973e632f480adf40775354d0
hatrari <ham.atrari@gmail.com>
2020-11-11 20:20:13 +0100

    add click event to hour box

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+function renderHourDigit(hour) {
+  return `0${hour}`.slice(-2);
+}
+
 function renderHourTemplate(hour) {
   return `
     <div class="hour">
-      <div>${hour}</div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
+      <div class="hour-label">${renderHourDigit(hour)}</div>
+      <div class="hour-box"></div>
+      <div class="hour-box"></div>
+      <div class="hour-box"></div>
+      <div class="hour-box"></div>
+      <div class="hour-box"></div>
+      <div class="hour-box"></div>
     </div>
   `;
 }
@@ -17,3 +21,23 @@ let container = document.getElementById('container');
 for(let i=0; i<24; i++) {
   container.innerHTML += renderHourTemplate(i);
 }
+
+function switchColor(div) {
+  let background = div.style.background;
+  if(background === 'white' || background === '') {
+    background = 'grey'
+  } else {
+    background = 'white'
+  }
+  div.style.background = background;
+}
+
+function addClickEventToHourBox() {
+  let divs = document.querySelectorAll('.hour-box');
+  divs.forEach(
+    item => 
+    item.addEventListener('click', () => switchColor(item))
+  );
+}
+
+document.addEventListener('DOMContentLoaded', addClickEventToHourBox);
